28e825df6d3649da9bcccd3f4beee95a15d7d311
jobscq <714298653@qq.com>
2019-04-23 22:12:02 +0800

    bug

--- src/views/CreateOrder.vue
+++ src/views/CreateOrder.vue
@@ -259,7 +259,7 @@ export default {
             wx.chooseWXPay({
               timestamp: args.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
               nonceStr: args.nonceStr, // 支付签名随机串，不长于 32 位
-              package: `prepay_id=${args.repay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+              package: `${args.repay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
               signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
               paySign: args.paySign, // 支付签名
               success(result) {
