79975c8f21f8f3a791a488bf695e765d45cf2616
jobscq <714298653@qq.com>
2019-04-23 22:26:42 +0800

    bug

--- src/views/CreateOrder.vue
+++ src/views/CreateOrder.vue
@@ -241,6 +241,9 @@ export default {
             loadingType: 'spinner',
             message: '加载中',
           });
+
+          // const submitResult = await this.$postData('/store/submitOrder', this.$data.formData);
+          // if(submitResult)
           // 这里如果后端要url 是#前面的部分不包括#号
           const res = await this.$getData('wx/js/sdk/init');
           wx.config({
@@ -255,8 +258,10 @@ export default {
             orderNo: this.formData.orderCode,
           });
           const args = data;
+          alert(args)
           wx.ready(() => {
             wx.chooseWXPay({
+              appId: args.appId,
               timestamp: args.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
               nonceStr: args.nonceStr, // 支付签名随机串，不长于 32 位
               package: `${args.repay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
